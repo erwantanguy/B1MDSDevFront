1f062e344f0bc05b00f69bc19cd6111c0a83c7d5
erwantanguy <erwan@ticoet.me>
2020-02-04 11:35:11 +0100

    maj js exercices 1, 2, 3 ,4 et 5

--- monjs.js
+++ monjs.js
@@ -4,12 +4,67 @@ $(document).ready(function(){
   for ( var i = 0; i < 5; i++ ) {
     $( '<div class="col-1">' ).appendTo( '#effets' );
   }
- $( "#effets div" ).click(function() {
-  $( this ).hide( 2000, function() {
-    $( this ).remove();
-  });
-});
+    $('#test').click(function() {
+      $( "#effets div" ).show(2000);
+      $( "#tests div" ).show(2000);
+    });
+     $( "#effets div" ).click(function() {
+      $( this ).hide(2000, function() {
+        //$( this ).remove();
+      });
+    });
+
+    $('#box1').click(function(){
+        $(this).css('background-color','pink').hide(4000);
+    });
+    $('#box2').click(function(){
+        $(this).css('background-color','purple').fadeOut(2000);
+    });
+    $('#box3').click(function(){
+        $(this).css('background-color','blue').slideUp(4000);
+    });    
     
+    $('#animate div').click(function(){
+        $(this).animate({
+            "left":"300px"
+        }, 3000);
+        $(this).animate({
+            "top":"-300px"
+        }, 4000).css('background-color','yellow');
+        
+//        $(this).animate({
+//            "left":"300px",
+//            "top":"-300px"
+//        }, 2000);
+        
+    });
     
-    
+    $('li:nth-child(4)').addClass('alerte');
+    $('#no-alerte').click(function(){
+        $('li').removeClass();
+    });
+    $('li a').click(function(){
+        $(this).parent('li').removeClass();
+    });
+    var i = $('li').length + 1;
+    console.log('i = ' + i);
+    //alert(i);
+    $('#plus').click(function(){
+        $('<li>ligne ' + i + '</li>').appendTo('ul');
+        i++;
+        if(i > 1){
+            $('#moins').show();
+        }
+        console.log(i);
+    });
+    $('#moins').click(function(){
+        if(i > 1){
+            $('li:last').remove();
+            i--;
+            if(i == 1){
+                $(this).hide();
+            }
+        }
+        console.log(i);
+    });
 });
