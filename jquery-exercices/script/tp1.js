9c52d97d844e733345dc0a3d2458260141c8c885
erwantanguy <erwan@ticoet.me>
2020-03-03 17:08:04 +0100

    new report

--- jquery-exercices/script/tp1.js
+++ jquery-exercices/script/tp1.js
@@ -1,6 +1,7 @@
 /*
  *	Formation jQuery
- *	TP 1 : Sélection et style*/
+ *	TP 1 : Sï¿½lection et style
+*/
 
 // Encapsulation :
 (function ($) {
@@ -10,10 +11,29 @@
 	
 		/*
 			EXEMPLE :
-			Aligner à gauche la première ligne du <thead>
+			Aligner ï¿½ gauche la premiï¿½re ligne du <thead>
 		*/
-		
-		// Sélection des 1eres cases avec le sélecteur CSS "thead th:first-child"
+               
+              //AJOUTER une ligne
+		var illeetvilaine = '<tr id="dep35"><td headers="reg23nom">Ille et Vilaine</td><td headers="reg23t4">45</td><td headers="reg23t3">23</td><td headers="reg23t2">80</td><td headers="reg23t1">60</td></tr>';               
+               $(illeetvilaine).appendTo($('#reg23'));
+               // changer Gironde par FinistÃ¨re
+               var test = $('#reg1 tbody tr:nth-child(2) td:first-child').text();
+               console.log(test);
+               $('#reg1 tbody tr:nth-child(2) td:first-child').text('Les B1 de MyDigitalSchool');
+               
+               //disparition
+               $('caption').click(function(){
+                   //$(this).siblings('tbody').children('tr:nth-child(3)').toggle();
+                   //$(this).parent().children('tbody').children('tr:nth-child(3)').toggle();
+                   $(this).parent().children('tbody').children('tr:nth-child(3)').toggleClass('grand');
+               });
+               
+               
+               
+               
+               
+		// Sï¿½lection des 1eres cases avec le sï¿½lecteur CSS "thead th:first-child"
 		var $premieresCasesThead = $("thead th:first-child") ;
 		
 		// On applique le style
@@ -21,12 +41,18 @@
 		
 		/*
 			EXERCICE 1 :
-			Dans chaque tableau, mettre la dernière ligne en italique
+			Dans chaque tableau, mettre la derniï¿½re ligne en italique
 		*/
-
+var derniereLigne = {
+    "font-style":"normal",
+    color:"#f00"
+};
+$("tbody tr:last-child").children('td').css(derniereLigne);
+//console.log(derniereLigne);
+//$("tbody tr:last-child").css('color','red').children('td').css('font-style','normal');
 		/*
 			EXERCICE 2 :
-			Dans le corps des tableaux, attribuer à la première cellule de chaque ligne le style suivant :
+			Dans le corps des tableaux, attribuer ï¿½ la premiï¿½re cellule de chaque ligne le style suivant :
 
 				{
 					color: #555555;
@@ -35,21 +61,33 @@
 				}			
 			
 			Utiliser pour cela le format JSON.
-			NB : dans l'ennoncé les styles sont écrits avec une syntaxe CSS, qui n'est pas celle que vous devez utiliser
+			NB : dans l'ennoncï¿½ les styles sont ï¿½crits avec une syntaxe CSS, qui n'est pas celle que vous devez utiliser
 			
 		*/
-		
+//		var jSonStyle = {
+//			textAlign:"left",
+//			fontWeight:"bold",
+//			color:"#555"
+//		};
+//                $('tbody td:first-child').css(jSonStyle);
+                $('tbody td:first-child').css({
+			textAlign:"left",
+			fontWeight:"bold",
+			color:"#555"
+		});
 		/*
 			EXERCICE 3 :
-			Attribuer à la ligne ayant l'identifiant reg24ref la couleur de fond #FBEC88
+			Attribuer ï¿½ la ligne ayant l'identifiant reg24ref la couleur de fond #FBEC88
 		*/
-		
+		$('#reg24ref').css('background-color','#FBEC88');
 		/*
 			EXERCICE 4 :
 			Attribuer aux lignes paires des tableaux (dans le tbody) la couleur de fond  #e8e8e8
 			Sauf si elles ont l'identifiant "#reg24ref"
 		*/
-				
-	}); // Fin des instructions envoyées au chargement de la page
+		$('tbody tr:not(#reg24ref):nth-child(even)').css('background-color','#e8e8e8');		
+                
+                
+	}); // Fin des instructions envoyï¿½es au chargement de la page
 	
 })(jQuery); // Fin de l'encapsulation
--- jquery-exercices/script/tp2.js
+++ jquery-exercices/script/tp2.js
@@ -1,6 +1,7 @@
 /*
  *	Formation jQuery
- *	TP 2 : Attributs, classes, contenus texte et html*/
+ *	TP 2 : Attributs, classes, contenus texte et html
+*/
 
 // Encapsulation :
 (function ($) {
@@ -25,24 +26,33 @@
 			EXERCICE 1
 			Mettre un attribut title valant "Provisoire" aux cellules qui ont la classe provisoire.
 		*/
-				
+	$('td.provisoire').attr('title','Provisoire');			
 		/*
 			EXERCICE 2
 			Dans les cellules vides, écrire « n.d » et ajouter la classe non-defini.
 		*/
-				
+	$('td:empty').text('n.d').addClass('non-defini');			
 		/*
 			EXERCICE 3
 			Mettre la classe non-significatif aux cellules dont l'attribut title contient "non significatif"
 		*/
-				
+	$('td[title*="non significatif"]').addClass('non-significatif');			
 		/*
 			EXERCICE 4
 			Récupérer le contenu texte du lien ayant l'identifiant id-ligne-active.
 			Ce contenu est l'identifiant de la ligne à laquelle vous devez mettre la classe class-ligne-active.
 			A la place du code, mettre le nom de la région active dans le lien ayant l'identifiant id-ligne-active.
 		*/
-		
+    var link = $('a#id-ligne-active');
+    //var id = link.text();
+    link.addClass('class-ligne-active');
+    //var cible = $('tr#'+id);
+    //var region = $('tr#'+id+' th:first-child').text();
+    //link.text(region);
+    
+    var id2 = link.attr('href');
+    var region2 = $(id2+' th:first-child').text();
+     link.text(region2);
 	}); // Fin des instructions envoyées au chargement de la page
 	
 })(jQuery); // Fin de l'encapsulation
