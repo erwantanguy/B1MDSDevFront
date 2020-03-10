5ea85dcca7ae14ab8266378f2ac7e69dfd03c3ea
erwantanguy <erwan@ticoet.me>
2020-03-10 13:30:14 +0100

    none

--- jquery-exercices/script/tp1.js
+++ jquery-exercices/script/tp1.js
@@ -1,7 +1,6 @@
 /*
  *	Formation jQuery
- *	TP 1 : Sï¿½lection et style
-*/
+ *	TP 1 : Sélection et style*/
 
 // Encapsulation :
 (function ($) {
@@ -11,29 +10,10 @@
 	
 		/*
 			EXEMPLE :
-			Aligner ï¿½ gauche la premiï¿½re ligne du <thead>
+			Aligner à gauche la première ligne du <thead>
 		*/
-               
-              //AJOUTER une ligne
-		var illeetvilaine = '<tr id="dep35"><td headers="reg23nom">Ille et Vilaine</td><td headers="reg23t4">45</td><td headers="reg23t3">23</td><td headers="reg23t2">80</td><td headers="reg23t1">60</td></tr>';               
-               $(illeetvilaine).appendTo($('#reg23'));
-               // changer Gironde par FinistÃ¨re
-               var test = $('#reg1 tbody tr:nth-child(2) td:first-child').text();
-               console.log(test);
-               $('#reg1 tbody tr:nth-child(2) td:first-child').text('Les B1 de MyDigitalSchool');
-               
-               //disparition
-               $('caption').click(function(){
-                   //$(this).siblings('tbody').children('tr:nth-child(3)').toggle();
-                   //$(this).parent().children('tbody').children('tr:nth-child(3)').toggle();
-                   $(this).parent().children('tbody').children('tr:nth-child(3)').toggleClass('grand');
-               });
-               
-               
-               
-               
-               
-		// Sï¿½lection des 1eres cases avec le sï¿½lecteur CSS "thead th:first-child"
+		
+		// Sélection des 1eres cases avec le sélecteur CSS "thead th:first-child"
 		var $premieresCasesThead = $("thead th:first-child") ;
 		
 		// On applique le style
@@ -41,18 +21,12 @@
 		
 		/*
 			EXERCICE 1 :
-			Dans chaque tableau, mettre la derniï¿½re ligne en italique
+			Dans chaque tableau, mettre la dernière ligne en italique
 		*/
-var derniereLigne = {
-    "font-style":"normal",
-    color:"#f00"
-};
-$("tbody tr:last-child").children('td').css(derniereLigne);
-//console.log(derniereLigne);
-//$("tbody tr:last-child").css('color','red').children('td').css('font-style','normal');
+
 		/*
 			EXERCICE 2 :
-			Dans le corps des tableaux, attribuer ï¿½ la premiï¿½re cellule de chaque ligne le style suivant :
+			Dans le corps des tableaux, attribuer à la première cellule de chaque ligne le style suivant :
 
 				{
 					color: #555555;
@@ -61,33 +35,21 @@ $("tbody tr:last-child").children('td').css(derniereLigne);
 				}			
 			
 			Utiliser pour cela le format JSON.
-			NB : dans l'ennoncï¿½ les styles sont ï¿½crits avec une syntaxe CSS, qui n'est pas celle que vous devez utiliser
+			NB : dans l'ennoncé les styles sont écrits avec une syntaxe CSS, qui n'est pas celle que vous devez utiliser
 			
 		*/
-//		var jSonStyle = {
-//			textAlign:"left",
-//			fontWeight:"bold",
-//			color:"#555"
-//		};
-//                $('tbody td:first-child').css(jSonStyle);
-                $('tbody td:first-child').css({
-			textAlign:"left",
-			fontWeight:"bold",
-			color:"#555"
-		});
+		
 		/*
 			EXERCICE 3 :
-			Attribuer ï¿½ la ligne ayant l'identifiant reg24ref la couleur de fond #FBEC88
+			Attribuer à la ligne ayant l'identifiant reg24ref la couleur de fond #FBEC88
 		*/
-		$('#reg24ref').css('background-color','#FBEC88');
+		
 		/*
 			EXERCICE 4 :
 			Attribuer aux lignes paires des tableaux (dans le tbody) la couleur de fond  #e8e8e8
 			Sauf si elles ont l'identifiant "#reg24ref"
 		*/
-		$('tbody tr:not(#reg24ref):nth-child(even)').css('background-color','#e8e8e8');		
-                
-                
-	}); // Fin des instructions envoyï¿½es au chargement de la page
+				
+	}); // Fin des instructions envoyées au chargement de la page
 	
 })(jQuery); // Fin de l'encapsulation
