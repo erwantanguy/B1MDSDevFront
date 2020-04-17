0641362f7cbc00850301cde6442c71ab3f106a84
erwantanguy <erwan@ticoet.me>
2020-04-17 21:22:07 +0200

    évaluation

---
+++ assets/js/src/vendor/react.jsx
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//const nom = "Sarrion";
+//console.log(nom);
+
+var nom = "Sarrion";
+console.log("avant le bloc : " + nom);
+
+if(true){
+    let nom = "Martin";
+    console.log("avant le bloc : " + nom);
+}
+//test
+
+console.log("avant le bloc : " + nom);
--- evaluation.js
+++ evaluation.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-    //$('div').hide(4000);
+//    console.log($('#trois article').width());
+    var $LesOnglets = $("#onglets li") ;
+    $LesOnglets.click(function(){
+        $LesOnglets.removeClass("active");
+        $(this).addClass("active");
+        return false;
+    });
+    $tables = $("#trois,#videos,#table").css("display","none") ;
+    $LesOnglets.click(function() {
+	var $clicked = $(this) ;
+        $tables.css("display","none") ;
+        selecteur = $clicked.attr("data-cible") ;
+        $tables.filter(selecteur).css("display","flex") ;
+        // $tables.css("display","none").filter($(this).attr("data-cible")).css("display","block") ;
+        return false ;
+    });
+    $LesOnglets.eq(0).click() ;
+    $('#trois article').height($('#trois article').width());
+    $('#videos nav li').click(function(){
+        var video = $(this).attr('data-id');
+//        console.log(video);
+        var urlVideo = "https://www.youtube.com/embed/"+video;
+//        console.log(urlVideo);
+//        console.log($('#videos article iframe').attr('src'));
+        $('#videos article iframe').attr('src', urlVideo);
+    });
+    var $loupe = $("<div id=\"loupe\"></div>").css("display","none").appendTo($("#table"));
+    $("td").hover(
+        function(){
+            $loupe.css("display","block").text($(this).text());
+        },
+        function(){
+            $loupe.css("display","none");
+        }
+    );
 });
